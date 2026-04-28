d54d254f8dbc3a832c3256a707800d951df49844
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2026-04-29 01:58:24 +0600

    styled header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import logo from "@/assets/logo.png";
+import Image from "next/image";
+import { format, compareAsc } from "date-fns";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="py-10 space-y-2 text-center">
+      <Image src={logo} width={400} height={100} alt="logo" className="mx-auto"></Image>
+      <p className="font-medium text-[1rem] text-gray-500">Journalism Without Fear or Favour</p>
+      <span className="font-medium text-[1rem] text-gray-500">
+        {format(new Date(), "EEEE, MMMM dd, yyyy")}
+      </span>
+    </div>
+  );
 };
 
 export default Header;
