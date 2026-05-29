945194f07ea01fe78d5e0fdef5ba3ba711ada9ee
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2026-05-29 12:05:11 +0600

    designed login page

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,7 +1,70 @@
-import React from "react";
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const LoginPage = () => {
-  return <div>Login</div>;
+  const [isShowPassword, setIsShowPassword] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
+
+  return (
+    <div className="container mx-auto min-h-[80vh] flex justify-center items-center bg-slate-100">
+      <div className="p-4 rounded-xl bg-white">
+        <h2 className="font-bold text-3xl text-center mb-6">Login your account</h2>
+
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Email</legend>
+            <input
+              type="email"
+              className="input"
+              placeholder="Type here email"
+              {...register("email", {
+                required: "Email field is required",
+              })}
+            />
+            {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+          </fieldset>
+          <fieldset className="fieldset relative">
+            <legend className="fieldset-legend">Password</legend>
+            <input
+              type="password"
+              className="input"
+              placeholder="Type here password"
+              {...register("password", {
+                required: "Password field is required",
+              })}
+            />
+            <span
+              className="absolute right-2 top-4 cursor-pointer"
+              onClick={() => setIsShowPassword(!isShowPassword)}
+            >
+              {isShowPassword ? <FaEye /> : <FaEyeSlash />}
+            </span>
+            {errors.password && <p className="text-red-500">{errors.password.message}</p>}
+          </fieldset>
+
+          <button className="btn w-full bg-slate-800 text-white">Login</button>
+        </form>
+
+        <p className="mt-4">
+          Don't have an account?{" "}
+          <Link href={"/register"} className="text-blue-500">
+            Register
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
